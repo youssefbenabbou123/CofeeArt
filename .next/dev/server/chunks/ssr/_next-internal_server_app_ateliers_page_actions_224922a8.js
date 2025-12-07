bf95de1cefbb827718dabba6ec45446c973d6375
youssefbenabbou123 <youssef.benabbou24@gmail.com>
2025-12-07 20:08:28 +0100

    Remove .next folder after Next.js update

--- .next/dev/server/chunks/ssr/_next-internal_server_app_ateliers_page_actions_224922a8.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/ateliers/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_ateliers_page_actions_224922a8.js.map
--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__21e36690._.css",
-  "static/chunks/node_modules_387c404d._.js",
-  "static/chunks/_a52b6ff8._.js"
-],
-    source: "dynamic"
-});
